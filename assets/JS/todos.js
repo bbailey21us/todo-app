e560ab1b9e796a1b6f89aaec8f38d4add93f16ca
bbailey21us <brandonbailey21@gmail.com>
2018-06-06 15:03:56 -0400

    finished the styling and added a background

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -14,6 +14,10 @@ $('input[type="text"]').keypress(function(event){
     if (event.which===13){
       var todoText = $(this).val();
       $(this).val("");
-      $("ul").append("<li><span>X</span> "+todoText+"</li>")
+      $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+todoText+"</li>")
     }
 });
+
+$("#plus").click(function(){
+    $('input[type="text"]').fadeToggle();
+})
